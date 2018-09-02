e5ac0ba3a9f6b8899792723dea70c43fde3be4dc
Dankiest Raft <daniel.yahalom@gmail.com>
2018-09-02 02:05:46 -0500

    initial commit
    This commit has a basic skeleton with a login menu and an onlineScene that is able to distinguish between the host and players

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WIJAA
+{
+	public class GameManager : NetworkBehaviour {
+
+		public override void OnStartClient()
+		{
+			if(isServer && isClient)
+			{
+				Debug.Log("Logged in as Host");
+				foreach(var gObject in GameObject.FindGameObjectsWithTag("Player")) //we want to disable all objects that aren't meant for the host. IE- meant for the player
+				{
+					gObject.SetActive(false);
+				}
+			}
+			else if(isClient)
+			{
+				Debug.Log("Logged in as Player");
+				foreach(var gObject in GameObject.FindGameObjectsWithTag("Host"))
+				{
+					gObject.SetActive(false);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/scripts/NetworkManagerWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace WIJAA
+{
+	/// <summary>
+	/// This is a class that makes it easier to make common calls to classes in NetworkManager
+	/// </summary>
+	public class NetworkManagerWrapper : MonoBehaviour {
+
+		public void SetIP(string newIP)
+		{
+			FindObjectOfType<NetworkManager>().networkAddress = newIP;
+		}
+
+		public void ConnectToGame()
+		{
+			FindObjectOfType<NetworkManager>().StartClient();
+		}
+
+		public void HostGame()
+		{
+			FindObjectOfType<NetworkManager>().StartHost();
+		}
+
+		string ipText;
+		public Text ipTextBox;
+		void Start()
+		{
+			ipText = Network.player.ipAddress;
+
+			if(ipTextBox != null)
+				ipTextBox.text = "IP: " + ipText;
+		}
+	}
+}
